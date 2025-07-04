d3ff0dfd5d9c6ee5e27b774e33593a5c610d44c6
ZidanBagus <zsetiawan02@gmail.com>
2025-07-04 20:54:36 +0700

    fix

--- backend-main/backend-main/models/index.js
+++ backend-main/backend-main/models/index.js
@@ -10,10 +10,12 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
+if (config && config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
+} else if (config) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
+} else {
+  throw new Error(`Database configuration for environment '${env}' not found`);
 }
 
 fs
